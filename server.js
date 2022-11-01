8c9220a0f990f82d494d46a9ea8a0aff6c41793c
Bryant Huynh <bryanthuynh@Bryants-MBP.lan>
2022-11-01 18:43:53 -0500

    updated server.js to get ports working

--- server.js
+++ server.js
@@ -1,20 +1,14 @@
-// Dependencies
 const express = require('express');
 
-// Point Server to the route files
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
-// Create an express server
 const app = express();
 
-// Set PORT
 const PORT = process.env.PORT || 3001;
 
-// Parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 
-// Parse incoming JSON data
 app.use(express.json());
 
 
@@ -22,7 +16,6 @@ app.use(express.static('public'));
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
-// Listener
 app.listen(PORT, () => {
     console.log(`API server is ready on port ${PORT}!`);
 });
