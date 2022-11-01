298613d88e7a75afd76de10d67388105dece8d72
Bryant Huynh <bryanthuynh@Bryants-MBP.lan>
2022-11-01 18:44:42 -0500

    added htmlRoutes.js

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,8 +1,6 @@
-// Dependencies
 const path = require('path');
 const router = require('express').Router();
 
-// This has to be on the top than the other routers
 router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'));
 });
@@ -11,10 +9,8 @@ router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
-// If no matching route is found default to home page
 router.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 });
-
 
 module.exports = router;
